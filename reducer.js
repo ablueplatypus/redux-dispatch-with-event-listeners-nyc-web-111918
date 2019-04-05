d96af0ee24d66c592f6774dca3ea5359ca393f3d
Matt Doering <ablueplatypus21@gmail.com>
2019-04-04 21:26:34 -0400

    completed lesson

--- reducer.js
+++ reducer.js
@@ -1,1 +1,30 @@
-// add code snippets from README
+let state; //= {count: 0}
+
+function reducer(state= {count: 0}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+}
+
+function dispatch(action){
+    state = reducer(state, action)
+    render()
+}
+
+function render(){
+    let container = document.querySelector('#container');
+    container.textContent = state.count;
+}
+
+dispatch({type: 'beef'})
+
+let button = document.querySelector('#button');
+
+button.addEventListener('click', () => {
+    // here the dispatch is resetting state. like setState would in React.
+    //The state is being set to the action type that we would like to update. 
+    dispatch({type: 'INCREASE_COUNT'})
+})
